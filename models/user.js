3f23543a9d2034e57b952f0e45178e9ab38f1e9f
User Name <user@email.com>
2025-02-19 12:30:14 -0500

    modified landing page to skyrockit

--- models/user.js
+++ models/user.js
@@ -1,6 +1,28 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+const applicationSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],
+  },
+});
+
+
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -9,8 +31,12 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationSchema], // embedding the applicationSchema here
 });
+
+
 
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
+
