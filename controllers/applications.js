76b3a442bf4382f296aea2d57e7d8f8631490b14
User Name <user@email.com>
2025-02-20 12:51:39 -0500

    add post route for applications

--- controllers/applications.js
+++ controllers/applications.js
@@ -28,7 +28,25 @@ router.get('/' , async (req,res) => {
 // adding new route and a create function
 
 router.get('/new', async (req,res) => {
+   
+
     res.render('applications/new.ejs');
 });
-router.put('/new', async (req,res) => {})
+
+
+router.post('/', async (req,res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+
+        currentUser.applications.push(req.body);
+    
+        await currentUser.save();
+
+        res.redirect(`/users/${currentUser._id}/applications`);
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+    
+});
 
